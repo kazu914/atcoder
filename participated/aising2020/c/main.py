0397c9799d161f54dd9e378434cdf97569cf2126
kazu914 <nkz0914ssk@gmail.com>
2020-07-12 11:15:07 +0900

    updated

--- participated/aising2020/c/main.py
+++ participated/aising2020/c/main.py
@@ -15,27 +15,16 @@ def LI(): return list(map(int, sys.stdin.readline().split()))
 
 def main():
     N = II()
-    ans = 0
-    list_counter = []
+    list_counter = collections.Counter()
 
     for i in range(1, math.floor(N**(1/2))+1):
-        for j in range(i,  math.floor(N**(1/2))+1):
-            for k in range(j,  math.floor(N**(1/2))+1):
-                n = str(i**2 + j**2 + k**2 + i*j + j*k + k*i)
-                tmp = set([i, j, k])
+        for j in range(1,  math.floor(N**(1/2))+1):
+            for k in range(1,  math.floor(N**(1/2))+1):
+                n = i**2 + j**2 + k**2 + i*j + j*k + k*i
+                list_counter.update({n: 1})
 
-                if len(tmp) == 3:
-                    tmp_list = [n]*6
-                if len(tmp) == 2:
-                    tmp_list = [n]*3
-                if len(tmp) == 1:
-                    tmp_list = [n]*1
-                list_counter.extend(tmp_list)
-
-    counter = collections.Counter(list_counter)
     for n in range(1, N+1):
-        print(counter[str(n)])
-
+        print(list_counter[n])
 
 
     # oj t -c "pypy3 main.py"
