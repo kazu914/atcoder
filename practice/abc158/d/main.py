76faa178066e26b4b36bcffd1b62ceb578c10b80
kazu914 <nkz0914ssk@gmail.com>
2020-07-19 14:32:19 +0900

    added abc157

--- practice/abc158/d/main.py
+++ practice/abc158/d/main.py
@@ -1,38 +1,40 @@
 #!/usr/bin/env python3
 import sys
-import collections as cl
 
 
-def II(): return int(sys.stdin.readline())
+def II():
+    return int(sys.stdin.readline())
 
 
-def MI(): return map(int, sys.stdin.readline().split())
+def MI():
+    return map(int, sys.stdin.readline().split())
 
 
-def LI(): return list(map(int, sys.stdin.readline().split()))
+def LI():
+    return list(map(int, sys.stdin.readline().split()))
 
 
 def main():
     S = input()
     Q = II()
-    
+
     direction = 1
     pre = []
     sur = []
 
     for i in range(Q):
         query = input()
-        if query == '1':
+        if query == "1":
             direction *= -1
         else:
             _, f, c = query.split()
             if direction == 1:
-                if f == '1':
+                if f == "1":
                     pre.append(c)
                 else:
                     sur.append(c)
             else:
-                if f == '1':
+                if f == "1":
                     sur.append(c)
                 else:
                     pre.append(c)
@@ -42,5 +44,6 @@ def main():
     else:
         ans = "".join(sur[::-1]) + "".join(S[::-1]) + "".join(pre)
     print(ans)
+
 
 main()
