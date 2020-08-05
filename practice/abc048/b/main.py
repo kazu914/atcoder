c70e960febe6aa23effb2ad7c0ef7b4bf3648171
kazu914 <nkz0914ssk@gmail.com>
2020-08-06 08:58:43 +0900

    abc048 b

---
+++ practice/abc048/b/main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import collections as cl
+import sys
+
+
+def II():
+    return int(sys.stdin.readline())
+
+
+def MI():
+    return map(int, sys.stdin.readline().split())
+
+
+def LI():
+    return list(map(int, sys.stdin.readline().split()))
+
+
+def main():
+    a, b, x = MI()
+    print(b // x - (a - 1) // x)
+
+
+main()
