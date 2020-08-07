09af5f04c2b9fa0be4142c1a5958b00a97a7ce81
kazu914 <nkz0914ssk@gmail.com>
2020-08-07 12:12:08 +0900

    added abc130d

--- practice/abc130/d/main.py
+++ practice/abc130/d/main.py
@@ -3,17 +3,34 @@ import sys
 import collections as cl
 
 
-def II(): return int(sys.stdin.readline())
+def II():
+    return int(sys.stdin.readline())
 
 
-def MI(): return map(int, sys.stdin.readline().split())
+def MI():
+    return map(int, sys.stdin.readline().split())
 
 
-def LI(): return list(map(int, sys.stdin.readline().split()))
+def LI():
+    return list(map(int, sys.stdin.readline().split()))
 
 
 def main():
-    N = II()
+    n, k = MI()
+    a = LI()
+    r = 0
+    ans = 0
+    ttl = 0
+
+    for left in range(n):
+        while ttl < k and r < n:
+            ttl += a[r]
+            r += 1
+        if ttl < k:
+            break
+        ans += n - r + 1
+        ttl -= a[left]
+    print(ans)
 
 
 main()
