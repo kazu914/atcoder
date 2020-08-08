2b53cd0c00c519bed83718cf0ac75e619705f2d8
kazu914 <nkz0914ssk@gmail.com>
2020-08-08 11:56:09 +0900

    added abc160 d

--- practice/abc160/d/main.py
+++ practice/abc160/d/main.py
@@ -3,17 +3,29 @@ import sys
 import collections as cl
 
 
-def II(): return int(sys.stdin.readline())
+def II():
+    return int(sys.stdin.readline())
 
 
-def MI(): return map(int, sys.stdin.readline().split())
+def MI():
+    return map(int, sys.stdin.readline().split())
 
 
-def LI(): return list(map(int, sys.stdin.readline().split()))
+def LI():
+    return list(map(int, sys.stdin.readline().split()))
 
 
 def main():
-    N = II()
+    n, x, y = MI()
+    ans = [0] * (n)
+
+    for i in range(1, n + 1):
+        for j in range(i + 1, n + 1):
+            dis = min(j - i, abs(i - x) + abs(j - y) + 1)
+            ans[dis] += 1
+
+    for i in range(1, n):
+        print(ans[i])
 
 
 main()
