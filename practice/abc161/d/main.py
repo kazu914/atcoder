7ebb8400c0dcbc4e04daffdb5526a96cdc7a52e9
kazu914 <nkz0914ssk@gmail.com>
2020-08-18 11:37:47 +0900

    abc 161 d

--- practice/abc161/d/main.py
+++ practice/abc161/d/main.py
@@ -15,5 +15,22 @@ def LI(): return list(map(int, sys.stdin.readline().split()))
 def main():
     N = II()
 
+    que = cl.deque(list(range(1, 10)))
+
+    while N > 1:
+        target = que.popleft()
+        if target % 10 != 0:
+            que.append(target*10 + (target % 10) - 1)
+
+        que.append(target*10 + (target % 10))
+
+        if target % 10 != 9:
+            que.append(target*10 + (target % 10) + 1)
+        N -= 1
+
+    ans = que.popleft()
+
+    print(ans)
+
 
 main()
