cda70075a323e3ba9b11b77d791adf68a3cc3142
kazu914 <nkz0914ssk@gmail.com>
2020-08-19 11:41:03 +0900

    added abc156 d

--- practice/abc156/d/main.py
+++ practice/abc156/d/main.py
@@ -1,19 +1,46 @@
 #!/usr/bin/env python3
-import sys
 import collections as cl
+import sys
 
 
-def II(): return int(sys.stdin.readline())
+def comb(n, r, mod=10 ** 9 + 7):
+    r = min(r, n-r)
+    upper = 1
+
+    for i in range(n, n-r, -1):
+        upper = (upper * i) % mod
+
+    lower = 1
+
+    for i in range(1, r+1):
+        lower = (lower * i) % mod
+
+    lower_inv = pow(lower, mod - 2, mod)
+
+    return (upper * lower_inv) % mod
 
 
-def MI(): return map(int, sys.stdin.readline().split())
+def II():
+    return int(sys.stdin.readline())
 
 
-def LI(): return list(map(int, sys.stdin.readline().split()))
+def MI():
+    return map(int, sys.stdin.readline().split())
+
+
+def LI():
+    return list(map(int, sys.stdin.readline().split()))
 
 
 def main():
-    N = II()
+    n, a, b = MI()
+
+    ttl = pow(2, n, 10**9+7) - 1
+
+    a_ttl = comb(n, a)
+    b_ttl = comb(n, b)
+
+    print((ttl - a_ttl - b_ttl) % (10**9+7))
 
 
 main()
