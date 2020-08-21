f3cc3ce3040836d6bc51578e95321607f39f1779
kazu914 <nkz0914ssk@gmail.com>
2020-08-21 09:59:36 +0900

    added abc160 e

--- practice/abc160/e/main.py
+++ practice/abc160/e/main.py
@@ -13,7 +13,28 @@ def LI(): return list(map(int, sys.stdin.readline().split()))
 
 
 def main():
-    N = II()
+    x, y, a, b, c = MI()
+    p = LI()
+    q = LI()
+    r = LI()
+
+    p.sort(reverse=True)
+    q.sort(reverse=True)
+    r.sort(reverse=True)
+
+    p_selected = p[:x]
+    q_selected = q[:y]
+
+    ans = [*p_selected, *q_selected]
+
+    ans.sort()
+    i = 0
+
+    while i < x + y and i < c and ans[i] < r[i]:
+        ans[i] = r[i]
+        i += 1
+
+    print(sum(ans))
 
 
 main()
