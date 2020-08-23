ad27edc8ca8b5188da0f93083bb94be8b71292d1
kazu914 <nkz0914ssk@gmail.com>
2020-08-23 11:34:55 +0900

    added abc166

--- practice/abc166/e/main.py
+++ practice/abc166/e/main.py
@@ -17,6 +17,21 @@ def LI():
 
 def main():
     N = II()
+    A = LI()
+
+    counter_add = cl.Counter()
+    counter_sub = cl.Counter()
+    for i in range(N):
+        add = i + 1 + A[i]
+        sub = i + 1 - A[i]
+        counter_add.update({str(add): 1})
+        counter_sub.update({str(sub): 1})
+
+    ans = 0
+    for key, val in counter_add.items():
+        ans += val * counter_sub[key]
+
+    print(ans)
 
 
 main()
