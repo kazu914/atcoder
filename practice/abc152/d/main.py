3b0cbf11f1fa7745438429bb4005f35f0429ffa3
kazu914 <nkz0914ssk@gmail.com>
2020-08-26 10:34:29 +0900

    added abc152 d

--- practice/abc152/d/main.py
+++ practice/abc152/d/main.py
@@ -14,6 +14,21 @@ def LI(): return list(map(int, sys.stdin.readline().split()))
 
 def main():
     N = II()
+    items = [[0 for i in range(9)]for j in range(9)]
+
+    for i in range(N+1):
+        start = int(str(i)[0])
+        end = int(str(i)[-1])
+        if end == 0:
+            continue
+        items[start - 1][end - 1] += 1
+
+    ans = 0
+    for i in range(9):
+        for j in range(9):
+            ans += items[i][j] * items[j][i]
+
+    print(ans)
 
 
 main()
