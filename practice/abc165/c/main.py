1c343005515c33c9e9006b07b51b5277a877e802
kazu914 <nkz0914ssk@gmail.com>
2020-09-03 10:56:54 +0900

    added abc165

--- practice/abc165/c/main.py
+++ practice/abc165/c/main.py
@@ -16,7 +16,31 @@ def LI():
 
 
 def main():
-    N = II()
+    N, M, Q = MI()
+    targets = [[] for i in range(Q)]
+    for i in range(Q):
+        target = LI()
+        targets[i] = target
+    ans = 0
+    for i in range(1, 2**(M+N)):
+        i_str = format(i, f"0{M+N-1}b")
+        A = []
+        tmp = 1
+        for k in range(len(i_str)):
+            if i_str[k] == "1":
+                tmp += 1
+            else:
+                A.append(tmp)
+        if len(A) != N:
+            continue
+        score = 0
+        for query in targets:
+            a, b, c, d = query
+            if A[b-1] - A[a-1] == c:
+                score += d
+        ans = max(ans, score)
+
+    print(ans)
 
 
 main()
